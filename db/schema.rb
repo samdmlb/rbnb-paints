d8276edebb6e79b63efd5062b85353ecb6e01f50
Erwan RAHIM <waren78.krahim@gmail.com>
2024-02-14 12:54:39 +0100

    added booking model with validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_13_172213) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_113758) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "paint_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["paint_id"], name: "index_bookings_on_paint_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "paints", force: :cascade do |t|
     t.string "name"
@@ -40,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_13_172213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "paints"
+  add_foreign_key "bookings", "users"
   add_foreign_key "paints", "users"
 end
