55e45c664d4a19e75936fe9ddc0b0e2c3fb65ea3
Erwan RAHIM <waren78.krahim@gmail.com>
2024-02-14 12:08:24 +0100

    added image in model

--- app/models/paint.rb
+++ app/models/paint.rb
@@ -1,6 +1,7 @@
 class Paint < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :artist, presence: true, length: { maximum: 30 }
