d8f1b611996acaf75227c1b1b7c74763bf098ca6
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-02-14 19:44:44 +0000

    ajout des names au model user et refonte de l'inscription/connexion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
+  has_many :paints
+
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 15 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 20 }
 end
