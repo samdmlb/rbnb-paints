1820d90b6e7c20e9265c22cc17ecec4ba6198c86
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-02-19 00:59:28 +0000

    ajout du choix de date avec flatpickr

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
 
   validates :user, presence: true
   validates :paint, presence: true
+  validates :start_date, comparison: { greater_than_or_equal_to: Date.today }
   validates :end_date, comparison: { greater_than: :start_date }
   validates :total_price, presence: true
 end
