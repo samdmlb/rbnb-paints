fcccee4e3445631a9d2d0e4cb22c5d167c75267f
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-02-22 15:32:59 +0000

    changement d'une image trop volumineuse

--- app/models/paint.rb
+++ app/models/paint.rb
@@ -5,8 +5,8 @@ class Paint < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :artist, presence: true, length: { maximum: 30 }
-  validates :movement, presence: true # ,inclusion: ["Impressionnisme", "Romantisme","Cubisme", "Art abstrait"]
-  validates :technique, presence: true # , inclusion: ["Aquarelle", "Peinture à l'huile"]
+  validates :movement, presence: true
+  validates :technique, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 999 }
   validates :year, presence: true,  numericality: { only_integer: true, greater_than_or_equal_to: 1000, less_than_or_equal_to: Date.current.year }
   validates :description, presence: true, length: { minimum: 15 }
@@ -15,6 +15,6 @@ class Paint < ApplicationRecord
   pg_search_scope :global_search,
                   against: %i[name artist movement technique],
                   using: {
-                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                    tsearch: { prefix: true }
                   }
 end
