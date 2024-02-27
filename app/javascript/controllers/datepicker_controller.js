ae536006223bb3cdb0bfed745645ffee363f618a
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-02-27 13:37:30 +0000

    impossible de réserver a des dates deja prises

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,11 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["startDate", "endDate"];
+
   connect() {
-    flatpickr(this.element, {
-      dateFormat: "d-m-Y"
-    })
+    this.initFlatpickr();
+  }
+
+  initFlatpickr() {
+    const reservedDates = this.data.get("reservedDates");
+    const disabledDates = reservedDates ? JSON.parse(reservedDates) : [];
+
+    flatpickr(this.startDateTarget, {
+      dateFormat: "d-m-Y",
+      disable: disabledDates,
+      minDate: "today"
+    });
+
+    flatpickr(this.endDateTarget, {
+      dateFormat: "d-m-Y",
+      disable: disabledDates,
+      minDate: "today"
+    });
   }
 }
